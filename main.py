59a223838d9e7fcbabb66203a1e73aa2f2858507
florianeBhz <behanzinf@gmail.com>
2021-08-24 00:19:24 +0200

    Ajout des commentaires dans GraphRec, User Modeling, Item Modeling

--- main.py
+++ main.py
@@ -102,6 +102,75 @@ if __name__ == "__main__":
     
     #data splitting 
 
+    ##training
+
+def train(model, device, train_loader, optimizer, epoch, best_rmse, best_mae):
+    model.train()
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        batch_nodes_u, batch_nodes_v, labels_list = data
+        optimizer.zero_grad()
+        loss = model.loss(batch_nodes_u.to(device), batch_nodes_v.to(device), labels_list.to(device))
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 100 == 0:
+            print('[%d, %5d] loss: %.3f, The best rmse/mae: %.6f / %.6f' % (
+                epoch, i, running_loss / 100, best_rmse, best_mae))
+            running_loss = 0.0
+    return running_loss
+
+
+def test(model, device, test_loader):
+    model.eval()
+    tmp_pred = []
+    target = []
+    with torch.no_grad():
+        for test_u, test_v, tmp_target in test_loader:
+            test_u, test_v, tmp_target = test_u.to(device), test_v.to(device), tmp_target.to(device)
+            val_output = model.forward(test_u, test_v)
+            tmp_pred.append(list(val_output.data.cpu().numpy()))
+            target.append(list(tmp_target.data.cpu().numpy()))
+    tmp_pred = np.array(sum(tmp_pred, []))
+    target = np.array(sum(target, []))
+    expected_mse = mean_squared_error(tmp_pred, target)
+    mae = mean_absolute_error(tmp_pred, target)
+    expected_rmse = mean_squared_error(expected_mse)
+    return expected_mse, expected_rmse, mae
+
+
+if __name__ == "__main__":
+
+    dir_data = './data/'
+    path_data = args.datadir+args.dataset+"_dataset.pickle"
+
+    if not os.path.exists(path_data):
+        preprocess(args.datadir, args.dataset,args.test)
+
+    
+    data_file = open(path_data, 'rb')
+    history_u_lists, history_ur_lists, history_v_lists, history_vr_lists, train_u, train_v, train_r, test_u, test_v, test_r, val_u, val_v, val_r, social_adj_lists, ratings_list = pickle.load(
+        data_file)
+
+    num_users = history_u_lists.__len__()
+    num_items = history_v_lists.__len__()
+    num_ratings = ratings_list.__len__()+1
+
+    print(num_ratings)
+
+    print(min(train_u),max(train_u))
+    print(min(train_v),max(train_v))
+
+    print(min(list(history_u_lists.keys())), max(list(history_u_lists.keys())))
+    print(min(list(social_adj_lists.keys())), max(list(social_adj_lists.keys())))
+
+    print(len(train_u), len(list(social_adj_lists.keys())))
+    print(len(np.intersect1d(list(history_u_lists.keys()), list(social_adj_lists.keys()))))
+    print(len( list(social_adj_lists.keys())),  len(list(history_u_lists.keys())))
+
+    
+    #data splitting 
+
     trainset = torch.utils.data.TensorDataset(torch.LongTensor(train_u), torch.LongTensor(train_v),
                                               torch.FloatTensor(train_r))
     testset = torch.utils.data.TensorDataset(torch.LongTensor(test_u), torch.LongTensor(test_v),
@@ -144,7 +213,7 @@ if __name__ == "__main__":
         train_losses.append(tr_loss)
 
         # please add the validation set to tune the hyper-parameters based on your datasets.
-        val_loss , val_rmse, val_mae = test(model, device, val_loader)
+        val_loss , val_rmse, val_mae = test(model, device, test_loader)
 
         # early stopping 
         if best_rmse > val_rmse:
@@ -158,6 +227,24 @@ if __name__ == "__main__":
         if endure_count > 5:
             break
 
+
+        """
+        best_rec_0 = max(recs[:, 0])
+        idx = list(recs[:, 0]).index(best_rec_0)
+
+        save_path = '%soutput/%s/%s.result' % (args['results_path'], args['dataset'])
+        f = open(save_path, 'a')
+
+        f.write(
+            'embed_size=%d, lr=%.4f, layer_size=%s, node_dropout=%s, mess_dropout=%s, regs=%s, adj_type=%s\n\t%s\n'
+            % (args['embed_size'], 0.0001, args['layer_size'], args['node_dropout'], args['mess_dropout'], args['regs'],
+            args['adj_type'], final_perf))
+        
+        f.close()
+        """
+    
+
+    
 
         """
         best_rec_0 = max(recs[:, 0])
